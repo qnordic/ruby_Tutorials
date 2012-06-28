73d11d5a7a81866ba2d76d2f67d2ff21033896c8
qnordic <chrisq@uw.edu>
2012-06-28 13:12:29 -0700

    First app push

---
+++ demo_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'a660bb06f04dd8b13f774e7ca3de4a18497de4b45bf38fa0487723a3ea0e58bc0730cdb0e8898eef63b246e9a843533965bb45f5de05210dabcff631e1e57065'
